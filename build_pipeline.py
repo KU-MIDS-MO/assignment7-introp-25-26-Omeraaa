ac73cf19aa84ad8946094aea7cea1792932a0b54
Ömer Boruç <omerboruc@Omer-MacBook-Pro.local>
2025-12-23 12:56:11 +0100

    Done

--- build_pipeline.py
+++ build_pipeline.py
@@ -1,4 +1,20 @@
 def build_pipeline(operation_names):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    valid_ops = ["square", "cube", "double", "triple"]
+    for op in operation_names:
+        if op not in valid_ops:
+            raise KeyError("Invalid Operation")
+    def pipeline(input_value):
+        for op in operation_names:
+            if op == "square":
+                input_value = input_value * input_value
+            elif op == "cube":
+                input_value = input_value * input_value * input_value
+            elif op == "double":
+                input_value = 2 * input_value
+            elif op == "triple":
+                input_value = 3 * input_value
+            else:
+                raise KeyError("Invalid Operation")
+        
+        return input_value
+    return pipeline
--- make_account.py
+++ make_account.py
@@ -1,4 +1,23 @@
 def make_account(initial_balance):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    balance = initial_balance
+    
+    def deposit(amount):
+        nonlocal balance
+        if amount < 0:
+            raise ValueError("Negative Number")
+        balance += amount
+        balance_after_deposit = balance
+        return balance_after_deposit
+
+    def withdraw(amount):
+        nonlocal balance
+        if amount <= 0:
+            raise ValueError("Amount must be positive")
+        if amount > balance:
+            raise ValueError("Not Enough Money")
+        balance -= amount
+        balance_after_withdraw = balance
+        return balance_after_withdraw
+
+    
+    return deposit, withdraw
